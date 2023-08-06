672fdeb39974bc1171d276a87e83850d30e6adfa
Chinwendu Agbaetuo <silviachinwendu@gmail.com>
2023-08-06 20:55:07 +0100

    environment setup

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Urbanist } from "next/font/google";
+
+const urbanist = Urbanist({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Omaricha",
+  description: "A beauty marketplace",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={urbanist.className}>{children}</body>
+    </html>
+  );
+}
