3f655822d6cece5984b84f9ba430ec8dec35194e
Chinwendu Agbaetuo <silviachinwendu@gmail.com>
2023-08-17 22:31:08 +0100

    fetch category from path name

---
+++ app/components/Navbar/Category/Categories.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import React from "react";
+import { Container } from "../Container";
+import { categories } from "@/app/utils/categories";
+import { CategoryBox } from "./CategoryBox";
+import { usePathname, useSearchParams } from "next/navigation";
+
+export const NavbarCategories = () => {
+  const params = useSearchParams();
+  const category = params?.get("category");
+  const pathname = usePathname();
+
+  const isMainPage = pathname === "/";
+
+  if (!isMainPage) {
+    return null;
+  }
+  return (
+    <Container>
+      <div className="pt-4 flex flex-row items-center gap-14 md:gap-2 justify-between overflow-x-scroll no-scrollbar overscroll-none">
+        {React.Children.toArray(
+          categories.map((item) => (
+            <CategoryBox
+              label={item.label}
+              selected={category === item.label}
+              icon={item.icon}
+            />
+          ))
+        )}
+      </div>
+    </Container>
+  );
+};
--- app/components/Navbar/Logo.tsx
+++ app/components/Navbar/Logo.tsx
@@ -5,7 +5,10 @@ import { useRouter } from "next/navigation";
 export const NavbarLogo = () => {
   const router = useRouter();
   return (
-    <div className="text-black font-bold text-2xl ">
+    <div
+      className="text-black font-bold text-2xl cursor-pointer"
+      onClick={() => router.push("/")}
+    >
       Omalicha
       <span className="w-2 h-2 bg-primary inline-flex items-center rounded-[3px]"></span>
     </div>
